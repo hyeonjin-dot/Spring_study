28685b76418b62b723edb8339974d8a92bae6eb2
hyeonjin-dot <guswls_2610@naver.com>
2024-12-06 18:03:15 +0900

    241206 spring basic 8

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,12 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -12,4 +19,8 @@ import java.awt.*;
                 Configuration.class})
 )
 public class AutoAppConfig {
+//    @Bean(name="memoryMemeberRepository")
+//    MemberRepository memoryMemeberRepository() {
+//        return new MemoryMemberRepository();
+ //   }
 }
