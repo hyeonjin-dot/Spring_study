49d7b54b218b709556645a6aedf5e9ce3de2a83f
hyeonjin-dot <guswls_2610@naver.com>
2025-03-22 22:18:35 +0900

    displayChange

--- core/src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
+++ core/src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.stereotype.Component;
 
 public class ComponentFilterAppConfigTest {
 
